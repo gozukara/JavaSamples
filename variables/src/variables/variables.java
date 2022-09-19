93616f8466ce7599bc6571acaf5a4d5a0311f51b
gozukara <47774119+gozukara@users.noreply.github.com>
2022-09-19 12:44:37 +0200

    first commit

---
+++ variables/src/variables/variables.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 10;
+		String mesaj = "Ögrenci sayisi";
+		System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
