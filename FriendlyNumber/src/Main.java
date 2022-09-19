091155f35a61bf3fe1bd460f816aef717a084a6b
gozukara <47774119+gozukara@users.noreply.github.com>
2022-09-19 12:40:34 +0200

    first commit

---
+++ FriendlyNumber/src/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 220-284
+
+		int number1 = 220;
+		int number2 = 284;
+		int total1 = 0;
+		int total2 = 0;
+
+		for (int i = 1; i < number1; i++) {
+			if (number1 % i == 0) {
+				total1 = total1 + i;
+
+			}
+
+		}
+		for (int i = 1; i < number2; i++) {
+			if (number2 % i == 0) {
+				total2 = total2 + i;
+
+			}
+
+		}
+		
+		
+		if (total1 == number2) {
+			System.out.println("Arkadas sayisidir");
+		} else {
+			System.out.println("arkadas sayi degildir");
+		}
+	}
+
+}
